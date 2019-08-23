e75a4734bfd36321ac046532f98a0f99affcc146
Seria <li12130@test.com>
2019-08-23 17:42:58 +0800

    代码进度2019/8/23 17:42

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -8,24 +8,79 @@ import {reqCategorys} from '../../api'
 class Category extends Component{
     //1.设计状态
     state = {
-        loading:false,//
+        loading:false,//标识是否正在加载中
         categorys:[],//一级分类列表
+        parentId:'0',//父分类的ID
+        subCategorys:[],//二级分类列表
+        parentName:'',//父分类的名称
+        showStatus:0//是否显示对话框0：都不显示，1：显示添加，2：显示更新
     };
-    //获取数据
+    //1.根据parentId获取数据
     getCategorys = async (parentId) => {
         //在发请求前，显示loading
         this.setState({loading:true});
-        const result = await reqCategorys('0');
+        //优先使用指定的parentId,如果没有指定使用状态中的parentId
+        parentId = parentId || this.state.parentId;
+        const result = await reqCategorys(parentId);
         //请求完成之后，隐藏loading
         this.setState({loading:false});
         console.log(result);
         if (result.data.status === 0) {
             const categorys = result.data.data;
-            this.setState({categorys});
+            if (parentId === '0') {
+                //更新一级分类列表
+                this.setState({categorys});
+            }else {
+                //更新二级分类列表
+                this.setState({
+                    subCategorys:categorys
+                })
+            }
         }else {
+            //获取列表失败
             message.error('获取分类列表失败');
         }
     };
+    //2.显示指定分类的子分类列表
+    showSubCates = (category) => {
+        // console.log('set之前',this.state.parentId);//0
+        //更新状态：state中的数据是异步更新(不会立即更新state中的数据)
+        this.setState({
+            parentId:category._id,
+            parentName:category.name
+        },() => {//在状态更新之后执行，在回调函数中能得到最新的状态数据
+            this.getCategorys();
+        })
+    };
+    //显示一级列表
+    showCategorys = () => {
+        this.setState({
+            parentId:'0',
+            parentName:'',
+            subCategorys:[],
+            showStatus:0,
+        })
+    };
+    //显示添加对话框
+    showAdd = () => {
+        this.setState({showStatus:1});
+    };
+    //显示修改的对话框
+    showUpdate = (category) => {
+        //保存category
+        this.category = category
+        //更新状态
+        this.setState({showStatus:2});
+    };
+    //添加分类
+    addCategory = async () => {
+        //得到数据
+        const {parentId, categoryName} = this.form.getFieldsValue();
+        //关闭对话框
+        this.setState({
+            showStatus:0
+        })
+    }
     //初始化Table的所有列数
     initColumns(){
         this.columns = [
